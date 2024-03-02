8413f2b729f5abd23ffb259d4e70b032211ce48d
unholy-sukuii <firojkchhetri1999@gmail.com>
2024-03-02 05:25:34 -0500

    applied mode toggler

--- app.js
+++ app.js
@@ -1,6 +1,164 @@
 /* ================================================= */
 /* ===============     Swiper JS here  ============== */
 /* ================================================= */
+
+class mode {
+  constructor(bgcolor, colour, accent, highlight) {
+    this.bgcolor = bgcolor;
+    this.colour = colour;
+    this.accent = accent;
+    this.highlight = highlight;
+  }
+
+
+}
+
+let cards = document.querySelectorAll(".card")
+let navbar = document.querySelector('#desktop-nav')
+let img = document.querySelectorAll("img");
+let h3 = document.querySelectorAll('h3')
+let links = document.querySelectorAll('a')
+let span = document.querySelectorAll('span')
+let smCard = document.querySelectorAll('.card-sm');
+let smcardHead = document.querySelector(".list h3");
+let paras = document.querySelectorAll('p')
+let headings = document.querySelectorAll('.headings')
+
+let darkMode = new mode("rgb(41, 41, 46)", "whitesmoke", `rgb(52, 49, 49)`, `rgb(136, 12, 12)`);
+let liteMode = new mode("#e6e1c5", "black", '#e6e1c5')
+
+let darkButton = document.querySelector("#dark");
+darkButton.style.cursor = 'pointer';
+let liteButton = document.querySelector("#lite")
+liteButton.style.cursor = 'pointer';
+
+let isDarkModeEnabled = false;
+const enableDark = () => {
+  document.querySelector('body').style.backgroundColor = darkMode.bgcolor
+  cards.forEach(card => {
+    card.style.backgroundColor = darkMode.bgcolor;
+    card.style.color = darkMode.colour;
+    console.log(darkMode.bgcolor)
+  })
+  img.forEach(image => {
+    image.style.filter = "grayscale(1)";
+  })
+  navbar.style.backgroundColor = darkMode.bgcolor
+  navbar.style.color = darkMode.colour
+
+  h3.forEach(h => {
+    h.style.color = darkMode.colour;
+  })
+  links.forEach(link => {
+    link.style.color = darkMode.colour;
+  })
+  span.forEach(Element => {
+    Element.style.color = darkMode.colour;
+  })
+  document.querySelector('#mainNews').style.color = darkMode.colour;
+  document.querySelector("#footer").style.backgroundColor = darkMode.bgcolor;
+  dayBar.style.color = darkMode.colour;
+  dateBar.style.color = darkMode.colour;
+  temp.style.color = darkMode.colour
+  timeBar.style.color = darkMode.colour;
+  smCard.forEach(card => {
+    card.style.backgroundColor = darkMode.accent;
+    card.style.color = darkMode.colour;
+  })
+  smcardHead.style.backgroundColor = darkMode.highlight;
+  paras.forEach(p => {
+    p.style.color = darkMode.colour;
+  })
+
+}
+
+const disableDark = () => {
+  document.querySelector('body').style.backgroundColor = '';
+  cards.forEach(card => {
+    card.style.backgroundColor = liteMode.bgcolor;
+    card.style.color = liteMode.colour;
+  });
+  img.forEach(image => {
+    image.style.filter = 'sepia(2';
+  });
+
+  navbar.style.backgroundColor = liteMode.bgcolor;
+  navbar.style.color = liteMode.colour;
+
+  h3.forEach(h => {
+    h.style.color = ' ';
+  });
+  links.forEach(link => {
+    link.style.color = '';
+  });
+
+  span.forEach(Element => {
+    Element.style.color = '';
+  });
+
+  paras.forEach(p => {
+    p.style.color = '';
+  });
+
+  document.querySelector('#mainNews').style.color = '';
+  document.querySelector("#footer").style.backgroundColor = '';
+
+  dayBar.style.color = '';
+  dateBar.style.color = '';
+  temp.style.color = '';
+  timeBar.style.color = '';
+
+  smCard.forEach(card => {
+    card.style.backgroundColor = '';
+    card.style.color = '';
+  });
+  smcardHead.style.backgroundColor = '';
+  cards.forEach(card => {
+    card.style.color = '';
+  });
+  headings.forEach(function (heading) {
+    heading.style.color = liteMode.colour;
+  })
+
+};
+
+let mobilenav = document.querySelector('#mobile-nav')
+let mobileDark = document.querySelector("#mobileDark")
+console.log(mobileDark)
+let counter = false;
+mobileDark.addEventListener('click',()=>{
+  if(counter){
+    mobileDark.innerText = "Dark"
+    mobilenav.style.backgroundColor = liteMode.bgcolor
+    mobilenav.style.color = liteMode.colour
+    disableDark()
+    counter = false;
+  }else{
+    mobileDark.innerText = "Lite"
+    mobilenav.style.backgroundColor = darkMode.bgcolor;
+    mobilenav.style.color = darkMode.colour;
+    enableDark()
+    counter = true;
+  }
+});
+darkButton.addEventListener('click', () => {
+  enableDark();
+  isDarkModeEnabled = true;
+
+})
+
+liteButton.addEventListener('click', () => {
+  disableDark();
+  isDarkModeEnabled = false;
+})
+
+
+
+
+
+/* ================================================= */
+/* ===============     Swiper JS here  ============== */
+/* ================================================= */
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 20,
@@ -124,6 +282,7 @@ function showTime() {
 }
 
 setInterval(showTime, 1000);
+console.log(Date)
 
 
 
@@ -379,171 +538,185 @@ let aroundNewsBottom = async () => {
 
 // aroundNewsBottom();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Dynamically pushing the business section 
+
+let mainNewsClutter = document.querySelector("#mainNews .container ")
+
+const mainNewsBlock = (articles) => {
+  mainNewsClutter.innerHTML = "";
+  articles.forEach((article, index) => {
+    const shortTitle = article.title.length > 35 ? article.title.slice(0, 35) + "..." : article.title;
+    const shortDes = article.description.length > 220 ? article.description.slice(0, 220) + "..." : article.description;
+    mainNewsClutter.innerHTML = `<div class="text-container"><h1>${shortTitle}</h1>
+      <p>
+        ${shortDes}
+      </p>
+    </div>
+    <img
+      src="${article.urlToImage}"
+      alt="" 
+    />`
+    document.querySelector("#mainNews .container img").addEventListener('click',()=>{
+      window.open(article.url,"_blank");
+    })
+
+  });
+
+  // Reapply dark mode styles
+  isDarkModeEnabled ? enableDark() : disableDark();
+};
+
+
+
+
+
+// Dynamically pushing the Around the world news bottom section
+
+let aroundNewsBottomClutter = document.querySelector("#more-around .container .main .right-container .bottom .flex-container")
+console.log(aroundNewsBottomClutter)
+
+const aroundNewsBottomBlock = (articles) => {
+  aroundNewsBottomClutter.innerHTML = "";
+  articles.forEach((article, index) => {
+    const shortTitle = article.title.length > 25 ? article.title.slice(0, 25) + "..." : article.title;
+    const shortDes = article.description.length > 50 ? article.description.slice(0, 50) + "..." : article.description;
+
+    const div = document.createElement('div');
+    div.classList.add("card");
+    div.innerHTML = `
+          <img src="${article.urlToImage}">
+          <h4>${shortTitle}</h4>
+          <span>${article.author}</span>
+      `;
+    aroundNewsBottomClutter.appendChild(div);
+
+    div.addEventListener('click', () => window.open(article.url, "_blank"));
+
+    // Add an additional class to the third card to hide it
+    if (index === 2) {
+      div.classList.add("hidden");
+      console.log
+    }
+    if (index === 5) {
+      div.classList.add("hidden");
+      console.log
+    }
+
+  });
+
+  // Reapply dark mode styles
+  isDarkModeEnabled ? enableDark() : disableDark();
+};
+
+
+// Dynamically pushing the sports section
+
+let sportsClutter = document.querySelector("#sports-pick .container .flex-container")
+
+const sportsBlock = (articles) => {
+  sportsClutter.innerHTML = "";
+  articles.forEach((article, index) => {
+    const shortTitle = article.title.length > 25 ? article.title.slice(0, 25) + "..." : article.title;
+    const shortDes = article.description.length > 120 ? article.description.slice(0, 120) + "..." : article.description;
+
+    const div = document.createElement('div');
+    div.classList.add("card");
+    div.innerHTML = `
+          <img src="${article.urlToImage}">
+          <h4>${shortTitle}</h4>
+          <p>${shortDes}</p>
+      `;
+    sportsClutter.appendChild(div);
+
+    div.addEventListener('click', () => window.open(article.url, "_blank"));
+
+    // Add an additional class to the third card to hide it
+    if (index === 2) {
+      div.classList.add("hidden");
+      console.log
+    }
+  });
+
+  // Reapply dark mode styles
+  isDarkModeEnabled ? enableDark() : disableDark();
+};
+
+//  Dynamically pushing the business section 
+
+let businessClutter = document.querySelector("#business .container .flex-container")
+
+const businessBlock = (articles) => {
+  businessClutter.innerHTML = "";
+  articles.forEach((article, index) => {
+    const shortTitle = article.title.length > 25 ? article.title.slice(0, 25) + "..." : article.title;
+    const shortDes = article.description.length > 120 ? article.description.slice(0, 120) + "..." : article.description;
+
+    const div = document.createElement('div');
+    div.classList.add("card");
+    div.innerHTML = `
+          <img src="${article.urlToImage}">
+          <h4>${shortTitle}</h4>
+          <p>${shortDes}</p>
+      `;
+    businessClutter.appendChild(div);
+
+    div.addEventListener('click', () => window.open(article.url, "_blank"));
+
+    // Add an additional class to the third card to hide it
+    if (index === 2) {
+      div.classList.add("hidden");
+      console.log
+    }
+  });
+
+  // Reapply dark mode styles
+  isDarkModeEnabled ? enableDark() : disableDark();
+};
+
+// Dynamically pushing the finance section 
+
 let financeClutter = document.querySelector("#finance .container .flex-container")
 
 const financeBlock = (articles) => {
   financeClutter.innerHTML = "";
-  articles.forEach(function (article) {
-    let shortTitle = article.title.length > 25 ? article.title.slice(0, 25) + "..." : article.title;
-    let shortDes = article.description.length > 120 ? article.description.slice(0, 120) + "..." : article.description
-    let div = document.createElement('div')
-    div.classList.add("card")
-    let image = document.createElement('img')
-    image.src = article.urlToImage;
-    let headline = document.createElement('h4')
-    headline.textContent = shortTitle;
-    let description = document.createElement('p')
-    description.textContent = shortDes;
-    div.appendChild(image);
-    div.appendChild(headline)
-    div.appendChild(description)
-    financeClutter.appendChild(div)
-    div.addEventListener('click', () => {
-      window.open(article.url, "_blank");
-    })
+  articles.forEach((article, index) => {
+    const shortTitle = article.title.length > 25 ? article.title.slice(0, 25) + "..." : article.title;
+    const shortDes = article.description.length > 120 ? article.description.slice(0, 120) + "..." : article.description;
 
+    const div = document.createElement('div');
+    div.classList.add("card");
+    div.innerHTML = `
+          <img src="${article.urlToImage}">
+          <h4>${shortTitle}</h4>
+          <p>${shortDes}</p>
+      `;
+    financeClutter.appendChild(div);
 
-  })
-}
+    div.addEventListener('click', () => window.open(article.url, "_blank"));
 
-
-(async () => {
-  try {
-    let financeArticles = await financeNews()
-    financeBlock(financeArticles);
-    console.log(financeArticles)
-  } catch (err) {
-    console.log(err)
-  }
-})();
-
-
-
-
-
-
-
-
-
-class mode {
-  constructor(bgcolor, colour, accent, highlight) {
-    this.bgcolor = bgcolor;
-    this.colour = colour;
-    this.accent = accent;
-    this.highlight = highlight;
-  }
-
-
-}
-
-
-
-let cards = document.querySelectorAll(".card")
-let navbar = document.querySelector('#desktop-nav')
-let img = document.querySelectorAll("img");
-let h3 = document.querySelectorAll('h3')
-let links = document.querySelectorAll('a')
-let span = document.querySelectorAll('span')
-let smCard = document.querySelectorAll('.card-sm');
-let smcardHead = document.querySelector(".list h3");
-let paras = document.querySelectorAll('p')
-let headings = document.querySelectorAll('.headings')
-
-
-let darkMode = new mode("rgb(41, 41, 46)", "whitesmoke", `rgb(52, 49, 49)`, `rgb(136, 12, 12)`);
-let liteMode = new mode("#e6e1c5", "black", '#e6e1c5')
-
-let darkButton = document.querySelector("#dark");
-darkButton.style.cursor = 'pointer';
-let liteButton = document.querySelector("#lite")
-liteButton.style.cursor = 'pointer';
-
-const enableDark = () => {
-  document.querySelector('body').style.backgroundColor = darkMode.bgcolor
-  cards.forEach(card => {
-    card.style.backgroundColor = darkMode.bgcolor;
-    card.style.color = darkMode.colour;
-    console.log(darkMode.bgcolor)
-  })
-  img.forEach(image => {
-    image.style.filter = "grayscale(1)";
-  })
-  navbar.style.backgroundColor = darkMode.bgcolor
-  navbar.style.color = darkMode.colour
-
-  h3.forEach(h => {
-    h.style.color = darkMode.colour;
-  })
-  links.forEach(link => {
-    link.style.color = darkMode.colour;
-  })
-  span.forEach(Element => {
-    Element.style.color = darkMode.colour;
-  })
-  document.querySelector('#mainNews').style.color = darkMode.colour;
-  document.querySelector("#footer").style.backgroundColor = darkMode.bgcolor;
-  dayBar.style.color = darkMode.colour;
-  dateBar.style.color = darkMode.colour;
-  temp.style.color = darkMode.colour
-  timeBar.style.color = darkMode.colour;
-  smCard.forEach(card => {
-    card.style.backgroundColor = darkMode.accent;
-    card.style.color = darkMode.colour;
-  })
-  smcardHead.style.backgroundColor = darkMode.highlight;
-  paras.forEach(p => {
-    p.style.color = darkMode.colour;
-  })
-}
-
-const disableDark = () => {
-  document.querySelector('body').style.backgroundColor = '';
-  cards.forEach(card => {
-    card.style.backgroundColor = liteMode.bgcolor;
-    card.style.color = liteMode.colour;
-  });
-  img.forEach(image => {
-    image.style.filter = 'sepia(2';
+    // Add an additional class to the third card to hide it
+    if (index === 2) {
+      div.classList.add("hidden");
+      console.log
+    }
   });
 
-  navbar.style.backgroundColor = liteMode.bgcolor;
-  navbar.style.color = liteMode.colour;
-
-  h3.forEach(h => {
-    h.style.color = ' ';
-  });
-  links.forEach(link => {
-    link.style.color = '';
-  });
-
-  span.forEach(Element => {
-    Element.style.color = '';
-  });
-
-  paras.forEach(p => {
-    p.style.color = '';
-  });
-
-  document.querySelector('#mainNews').style.color = '';
-  document.querySelector("#footer").style.backgroundColor = '';
-
-  dayBar.style.color = '';
-  dateBar.style.color = '';
-  temp.style.color = '';
-  timeBar.style.color = '';
-
-  smCard.forEach(card => {
-    card.style.backgroundColor = '';
-    card.style.color = '';
-  });
-  smcardHead.style.backgroundColor = '';
-  cards.forEach(card => {
-    card.style.color = '';
-  });
-  headings.forEach(function (heading) {
-    heading.style.color = liteMode.colour;
-  })
-
+  // Reapply dark mode styles
+  isDarkModeEnabled ? enableDark() : disableDark();
 };
 
 
@@ -552,11 +725,41 @@ const disableDark = () => {
 
 
 
-darkButton.addEventListener('click', () => {
-  enableDark();
 
-})
 
-liteButton.addEventListener('click', () => {
-  disableDark();
-})
+
+
+
+
+
+
+
+
+
+// (async () => {
+//   try {
+//     let financeArticles = await financeNews()
+//     let businessArticles = await businessNews()
+//     let sportsArticles = await sportNews()
+//     let mainNewsArticles = await mainNews()
+//     let aroundNewsBottomArticles = await aroundNewsBottom();
+
+//     mainNewsBlock(mainNewsArticles)
+//     aroundNewsBottomBlock(aroundNewsBottomArticles)
+//     sportsBlock(sportsArticles, "this is the one")
+//     financeBlock(financeArticles);
+//     businessBlock(businessArticles);
+//     // console.log(sportsArticles);
+//   } catch (err) {
+//     console.log(err)
+//   }
+// })();
+
+
+
+
+
+
+
+
+
